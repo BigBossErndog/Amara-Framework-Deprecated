087c35a31305fc4fe6c2d48a7787a5b2341ef47a
BigBossErndog <ernestplacido05@gmail.com>
2020-04-05 16:56:04 -0400

    Minor fixes.

--- amara/amara_pathFinding.cpp
+++ amara/amara_pathFinding.cpp
@@ -306,7 +306,7 @@ namespace Amara {
                 }
             }
         }
-
+        
         if (foundPath) {
             int current = targetId;
             while (current != -1) {
@@ -314,6 +314,9 @@ namespace Amara {
                 task->path.push_front(curTile);
                 current = curTile.parentId;
             }
+        }
+        else {
+            SDL_Log("Path not found: from (%d,%d) to (%d,%d)", task->startX, task->startY, task->targetX, task->targetY);
         }
         
         task->findingPath = false;
