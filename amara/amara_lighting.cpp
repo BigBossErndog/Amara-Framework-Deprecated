aca370e54658a71c715ad5c5b4a8a145052a2866
Ernest Placido <manage.bigbosserndog@gmail.com>
2020-05-24 19:45:41 -0400

    Fully implemented LightLayer.

--- amara/amara_lighting.cpp
+++ amara/amara_lighting.cpp
@@ -12,6 +12,8 @@ namespace Amara {
             SDL_Texture* lightTexture = nullptr;
             SDL_Color fillColor;
             SDL_Color recColor;
+            SDL_Rect destRect;
+            SDL_Rect viewport;
 
             SDL_BlendMode blendMode = SDL_BLENDMODE_MOD;
 
@@ -22,11 +24,21 @@ namespace Amara {
             int imageWidth = 0;
             int imageHeight = 0;
 
+            LightLayer(int r, int g, int b, int a) {
+                fillColor = {r, g, b, a};
+            }
+
+            LightLayer(int r, int g, int b): LightLayer(r, g, b, 255) {}
+
+            LightLayer(SDL_Color gColor) {
+                fillColor = gColor;
+            }
+
             void init(Amara::GameProperties* gProperties, Amara::Scene* gScene, Amara::Entity* gParent) {
                 Amara::Actor::init(gProperties, gScene, gParent);
                 gRenderer = properties->gRenderer;
 
-                blendMode = SDL_BLENDMODE_BLEND;
+                blendMode = SDL_BLENDMODE_MOD;
 
                 destroyTexture();
             }
@@ -56,19 +68,47 @@ namespace Amara {
                     height = vh;
                     createTexture();
                 }
+
+                if (alpha < 0) alpha = 0;
+                if (alpha > 1) alpha = 1;
+
+                viewport.x = vx;
+                viewport.y = vy;
+                viewport.w = vw;
+                viewport.h = vh;
+                SDL_RenderSetViewport(properties->gRenderer, &viewport);
+                
+                destRect.x = 0;
+                destRect.y = 0;
+                destRect.w = vw;
+                destRect.h = vh;
+
+                SDL_GetRenderDrawColor(gRenderer, &recColor.r, &recColor.g, &recColor.b, &recColor.a);
                 recTarget = SDL_GetRenderTarget(gRenderer);
-                resetPassOnProperties();
 
                 SDL_SetRenderTarget(gRenderer, lightTexture);
-                SDL_SetRenderDrawColor(properties->gRenderer, fillColor.r, fillColor.g, fillColor.b, fillColor.a);
+                SDL_SetRenderDrawColor(gRenderer, fillColor.r, fillColor.g, fillColor.b, fillColor.a);
+                SDL_SetTextureBlendMode(lightTexture, SDL_BLENDMODE_NONE);
+                SDL_RenderFillRect(gRenderer, NULL);
 
-                Amara::Actor::draw(vx, vy, vw, vh);
+                Amara::Actor::draw(0, 0, vw, vh);
 
-                SDL_SetRenderDrawColor(properties->gRenderer, recColor.r, recColor.g, recColor.b, recColor.a);
+                SDL_SetRenderDrawColor(gRenderer, recColor.r, recColor.g, recColor.b, recColor.a);
                 SDL_SetRenderTarget(gRenderer, recTarget);
 
-                SDL_SetTextureBlendMode(lightTexture, blendMode);
-			    SDL_SetTextureAlphaMod(lightTexture, alpha * properties->alpha * 255);
+                if (lightTexture != nullptr) {
+                    SDL_SetTextureBlendMode(lightTexture, blendMode);
+                    SDL_SetTextureAlphaMod(lightTexture, 255);
+                    SDL_RenderCopyEx(
+                        properties->gRenderer,
+                        lightTexture,
+                        NULL,
+                        &destRect,
+                        NULL,
+                        NULL,
+                        SDL_FLIP_NONE
+                    );
+                }
             }
     };
 }
